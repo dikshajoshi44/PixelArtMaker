c44722b00252ab52178ab24d7f6becd299555c79
dikshaj <dikshaj@qubole.com>
2018-02-18 22:24:38 +0530

    remove old table rows on submit

--- project/designs.js
+++ project/designs.js
@@ -4,7 +4,7 @@
 
 // When size is submitted by the user, call makeGrid()
 $("#submit_button").submit(function(event){
-    $("#pixelCanvas").remove();
+    $('#pixelCanvas tr').remove()
     var height = $('#inputHeight').val();
     var width = $('#inputWeight').val();
     makeGrid(height,width);
@@ -14,15 +14,15 @@ $("#submit_button").submit(function(event){
 
 function makeGrid(rows, cols) {
     // Your code goes here!
-    for(var i = 0 ; i<=rows ;i++){
+    for(var i = 1 ; i<= rows ; i++){
         $("#pixelCanvas").append("<tr></tr>");
-        for(var j=0 ; j<= cols ;j++){
+        for(var j = 1 ; j<= cols ; j++){
             $("tr:last").append("<th></th>");
             $("tr").addClass( "box" );
         }
     }
     console.log(event);
-    event.preventDefault();
+    // event.preventDefault();
 
 }
 $(".box").on("click", function(){
