0082cc30bdb434e169567c0acc6c0a7f7bd7b484
dikshaj <dikshaj@qubole.com>
2018-02-18 22:10:56 +0530

    added jquery lib

--- project/designs.js
+++ project/designs.js
@@ -3,11 +3,13 @@
 
 
 // When size is submitted by the user, call makeGrid()
-$("#submit_button").on( "submit", function(){
+$("#submit_button").submit(function(event){
     $("#pixelCanvas").remove();
     var height = $('#inputHeight').val();
     var width = $('#inputWeight').val();
     makeGrid(height,width);
+    event.preventDefault();
+    console.log(event);
 });
 
 function makeGrid(rows, cols) {
@@ -19,6 +21,8 @@ function makeGrid(rows, cols) {
             $("tr").addClass( "box" );
         }
     }
+    console.log(event);
+    event.preventDefault();
 
 }
 $(".box").on("click", function(){
